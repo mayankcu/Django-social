13bed0be1e95c27b194615d86bb69197165b24bd
Mayank Yadav <connectmayank@gmail.com>
2012-10-01 18:29:04 -0700

    adding database postgresql settings

--- settings.py
+++ settings.py
@@ -13,21 +13,22 @@ ROOT_PATH = abspath(dirname(__file__))
 PROJECT_NAME = basename(ROOT_PATH)
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ## ('Your Name', 'your_email@domain.com'),
 )
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hellodjangoDB',                      # Or path to database file if using sqlite3.
-        'USER': 'hellodjango',                      # Not used with sqlite3.
-        'PASSWORD': '12345',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+   #     'NAME': 'hellodjangoDB',                      # Or path to database file if using sqlite3.
+    #    'USER': 'hellodjango',                      # Not used with sqlite3.
+     ##  'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+       # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    #}
+#}
  
+ 
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 CACHES = {
     'default': {
